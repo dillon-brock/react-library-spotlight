7eb89671471c676bd65bf71bfc34959034902548
Dillon Brock <mdillon1297@gmail.com>
2022-09-29 17:09:21 -0700

    implemented Bulma to style navbar and titles for each page

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import PlantsTable from './components/PlantsTable/PlantsTable';
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/table">Table</Link>
-        <Link to="/charts">Charts</Link>
+      <nav className="navbar">
+        <Link to="/table" className="navbar-item">Table</Link>
+        <Link to="/charts" className="navbar-item">Charts</Link>
       </nav>
       <Switch>
         <Route path="/table">
-          <h1>Table Libary</h1>
+          <h1 className="title is-1">Table Libary</h1>
+          <h2 className="title is-3">Plants</h2>
           <PlantsTable />
         </Route>
         <Route path="/charts">
-          <h1>Chart Library</h1>
-          <h2>Percent of Population Identifying as Transgender By Race</h2>
-          <h4>data from <a href='https://williamsinstitute.law.ucla.edu/publications/trans-adults-united-states/'>UCLA Williams Institute</a></h4>
+          <h1 className="title is-1">Chart Library</h1>
+          <h2 className="title is-3">Percent of Population Identifying as Transgender By Race</h2>
+          <h4 className="title is-6">data from <a href='https://williamsinstitute.law.ucla.edu/publications/trans-adults-united-states/'>UCLA Williams Institute</a></h4>
           <GenderChart />
         </Route>
       </Switch>
